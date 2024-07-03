70ea76b6bd077b8b5ec2db299b7b9b302928eb43
Kobe <kimme143@umn.edu>
2024-07-02 23:58:54 -0500

    Refactor installation script

--- install.sh
+++ install.sh
@@ -6,10 +6,17 @@ make build
 # Move the executable to /usr/local/bin
 sudo mv iris /usr/local/bin
 
-# Add to path
-echo 'export PATH="$PATH:/path/to/your/script"' >> ~/.zshrc
+# Retrieve current directory PATH
+INSTALL_PATH=$(pwd)
 
-# Reload to apply changes
-source ~/.zshrc
+# Add the directory containing the iris executable to PATH in ~/.zshrc
+if ! grep -q "$INSTALL_PATH" ~/.zshrc; then
+    echo "export PATH=\"\$PATH:$INSTALL_PATH\"" >> ~/.zshrc
+    echo "Added $INSTALL_PATH to PATH in ~/.zshrc."
+    # Reload ~/.zshrc to apply changes
+    source ~/.zshrc
+else
+    echo "$INSTALL_PATH is already in PATH in ~/.zshrc."
+fi
 
 echo "Installation complete. You can now use the 'iris' command to invoke the Iris compiler."
