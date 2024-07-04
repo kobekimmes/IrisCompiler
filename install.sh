02ccd2fee1291c44cc7a10009c7a8ac835531408
Kobe <kimme143@umn.edu>
2024-07-04 11:56:53 -0500

    Add .gitignore and rename test file

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@
 make build
 
 # Move the executable to /usr/local/bin
-sudo mv iris /usr/local/bin
+sudo cp -r iris /usr/local/bin
 
 # Retrieve current directory PATH
-INSTALL_PATH=$(pwd)
+INSTALL_PATH=/usr/local/bin
 
 # Add the directory containing the iris executable to PATH in ~/.zshrc
 if ! grep -q "$INSTALL_PATH" ~/.zshrc; then
-    echo "export PATH=\"\$PATH:$INSTALL_PATH\"" >> ~/.zshrc
+    echo "export PATH=$PATH:$INSTALL_PATH" >> ~/.zshrc
     echo "Added $INSTALL_PATH to PATH in ~/.zshrc."
     # Reload ~/.zshrc to apply changes
     source ~/.zshrc
